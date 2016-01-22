658bad4d783b61798eb20d2bbb8b2c7bbae0f481
Kai Wang <kai.wang.nankai@gmail.com>
2016-01-22 23:22:49 +1100

    q133 edited

--- Q133 Longest Words.py
+++ Q133 Longest Words.py
@@ -1,3 +1,14 @@
+'''
+Thoughts:
+
+Use res to record the current longest word(s)
+Iterate through the dictionary, compare the current word with the words in res
+If length of current word is longer, re-initialise res with current word
+If lengths are equal, append current word to res
+If length of current word is shorter, ignore
+
+'''
+
 class Solution:
 	def longestWords(self, dictionary):
 		if not dictionary:
