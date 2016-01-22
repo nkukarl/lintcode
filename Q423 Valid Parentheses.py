7d99ad0ceb8d261e9f357d222c6c898fdf966dce
Kai Wang <kai.wang.nankai@gmail.com>
2016-01-22 23:42:53 +1100

    q423 edited

--- Q423 Valid Parentheses.py
+++ Q423 Valid Parentheses.py
@@ -1,3 +1,18 @@
+'''
+Thoughts:
+
+Iterate the chars in s:
+If char is a left bracket, push it to the stack
+If char is a right bracket, check
+	whether the stack is empty: is empty -> False
+	whether the top of the stack is a left bracket: not left -> False
+	whether the left and right bracket match each other: not match -> False
+Otherwise, pop the top element and continue
+
+When the iteration finishes, the stack shall be empty if the parentheses are valid
+
+'''
+
 class Solution:
 	def isValidParentheses(self, s):
 		left = {'(': 1, '[': 2, '{': 3}
