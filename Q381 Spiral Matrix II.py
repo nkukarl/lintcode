5c46a1b66d463eb59e25cd58a9d07abb51c685fe
Kai Wang <kai.wang.nankai@gmail.com>
2016-01-24 09:26:46 +1100

    q381 edited

--- Q381 Spiral Matrix II.py
+++ Q381 Spiral Matrix II.py
@@ -1,3 +1,13 @@
+'''
+Thoughts:
+
+nums contains all the numbers that shall be filled into the matrix
+Record the current top, bottom, left and right positions
+Use dir to determine which direction shall the spiral go
+dir plus 1 and mod 4 at the end of each iteration
+
+'''
+
 class Solution:
 	def generateMatrix(self, n):
 		nums = [i for i in range(1, n ** 2 + 1)]
@@ -6,18 +16,18 @@ class Solution:
 
 		top, bottom = 0, n - 1
 		left, right = 0, n - 1
-		direction = 0
+		dir = 0
 		
 		while top <= bottom or left <= right:
-			if direction == 0:
+			if dir == 0:
 				for col in range(left, right + 1):
 					matrix[top][col] = nums.pop()
 				top += 1
-			elif direction == 1:
+			elif dir == 1:
 				for row in range(top, bottom + 1):
 					matrix[row][right] = nums.pop()
 				right -= 1
-			elif direction == 2:
+			elif dir == 2:
 				for col in range(right, left - 1, -1):
 					matrix[bottom][col] = nums.pop()
 				bottom -= 1
@@ -25,7 +35,7 @@ class Solution:
 				for row in range(bottom, top - 1, -1):
 					matrix[row][left] = nums.pop()
 				left += 1
-			direction = (direction + 1) % 4
+			dir = (dir + 1) % 4
 
 		return matrix
 
