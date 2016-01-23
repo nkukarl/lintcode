a16027c884606b8e7d5221d9e336db04242cf1b5
Kai Wang <kai.wang.nankai@gmail.com>
2016-01-24 09:09:23 +1100

    q384 edited

--- Q384 Longest Substring Without Repeating Characters.py
+++ Q384 Longest Substring Without Repeating Characters.py
@@ -1,10 +1,20 @@
+'''
+Thoughts:
+
+Iterate through all the characters in s
+If current char is not in temporary array, append current char
+Else find the index of current char in the temporary and discard all the chars before it and itself, append current char
+Keep monitoring the length of the temporary array
+
+'''
+
 class Solution:
 	def lengthOfLongestSubstring(self, s):
 		tmp = []
 		maxLen = 0
 		for char in s:
 			if char not in tmp:
-				tmp += char
+				tmp += [char]
 			else:
 				tmp = tmp[tmp.index(char) + 1:] + [char]
 			maxLen = max(maxLen, len(tmp))
