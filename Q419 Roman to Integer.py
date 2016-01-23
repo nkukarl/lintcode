cd8a0fc75f373300251981b89052f9a41eb95b67
Kai Wang <kai.wang.nankai@gmail.com>
2016-01-23 23:23:02 +1100

    q419 edited

--- Q419 Roman to Integer.py
+++ Q419 Roman to Integer.py
@@ -1,3 +1,14 @@
+'''
+Thoughts:
+
+Initialise prev to None
+Store previous character and compare prev with cur
+If prev >= cur, add prev to result and update prev to cur
+If prev < cur, prev + cur would form a valid two roman letter integer, add this to res and reset prev to None
+After exiting the iteration, if prev != None, add prev to res
+
+'''
+
 Int2Roman = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100:'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
 keys, values = [], []
 for k, v in Int2Roman.items():
