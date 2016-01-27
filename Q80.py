74d22a02ac9ab9df3e0c4c749422bfc62075b366
Kai Wang <kai.wang.nankai@gmail.com>
2016-01-28 09:44:41 +1100

    q80 edited

--- Q80.py
+++ Q80.py
@@ -1,25 +1,43 @@
+'''
+Thoughts:
+
+A more difficult version of Find Kth Smallest Number
+If the number of elements in nums is odd, k = (n + 1) // 2
+Otherwise, k = n // 2
+
+Randomly choose a number from nums, let it be the pivot
+Use A1, A2 and A3 to record the numbers smaller than, greater than and equal to the pivot
+If len(A1) is greater than k - 1, find the kth smallest in A1
+If len(A1) + len(A3) is smaller than k, find the kth smallest in A2
+Otherwise, return pivot
+
+'''
+
 import random
+
 class Solution:
 	def median(self, nums):
-		if len(nums) % 2 == 0:
-			return self.helper(nums, len(nums) // 2)
-		return self.helper(nums, len(nums) // 2 + 1)
-
-	def helper(self, nums, k):
+		# write your code here
+		n = len(nums)
+		if n % 2 == 1:
+			return self.findKthSmallest(nums, (n + 1) // 2)
+		return self.findKthSmallest(nums, n // 2)
+		
+	def findKthSmallest(self, nums, k):
 		pivot = random.choice(nums)
-		nums1, nums2 = [], []
+		A1, A2, A3 = [], [], []
 		for n in nums:
 			if n < pivot:
-				nums1.append(n)
+				A1.append(n)
+			elif n > pivot:
+				A2.append(n)
 			else:
-				nums2.append(n)
-		if len(set(nums1 + nums2)) == 1:
-			return pivot
-		if len(nums1) == k - 1:
-			return pivot
-		if len(nums1) > k - 1:
-			return self.helper(nums1, k)
-		return self.helper(nums2, k - len(nums1))
+				A3.append(n)
+		if len(A1) > k - 1:
+			return self.findKthSmallest(A1, k)
+		if len(A1) + len(A3) < k:
+			return self.findKthSmallest(A2, k - (len(A1) + len(A3)))
+		return pivot
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8]
 random.shuffle(nums)
